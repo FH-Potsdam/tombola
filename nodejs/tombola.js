1373776161d614e118fa12fe7ca430a3b79466fe
fabiantheblind <icke@fabianmoronzirfas.me>
2016-02-02 11:00:06 +0100

    modified:   tombola.js

--- nodejs/tombola.js
+++ nodejs/tombola.js
@@ -4,20 +4,21 @@
 var fs = require('fs'); // inclide the filesystem module
 
 // this list was for testing purpose
-// var list = ["Duffy Duck","Bart Simpson","Bender Bending Rodríguez","Gabriel García Márquez","Massimo Banzi","J. Johna Jameson"];
+// var list = ["Duffy Duck","Bart Simpson","Bender Bending Rodríguez",
+// "Gabriel García Márquez","Massimo Banzi","J. Johna Jameson"];
 //
 // Read the names.txt file and split it into an array
-var list = fs.readFileSync("../_private/names.txt", 'utf8').split('\n');
+var list = fs.readFileSync('../_private/names.txt', 'utf8').split('\n');
 // console.log(lines);
 
 // shuffle function object
- var shuffler = function(array) {
+var shuffler = function(array) {
   // http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
   var currentIndex = array.length;
-    var temporaryValue;
-    var randomIndex;
+  var temporaryValue;
+  var randomIndex;
   // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
+  while (currentIndex !== 0) {
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
@@ -31,21 +32,27 @@ var list = fs.readFileSync("../_private/names.txt", 'utf8').split('\n');
 };
 
 // some logging
-console.log("\n------------\nOriginal List:\n" + list +"\n");
+console.log('\n------------\nOriginal List:\n' + list + '\n');
 shuffler(list);
-console.log("Shuffeld List:\n"+ list + "\n");
+console.log('Shuffeld List:\n' + list + '\n');
 
 // sorry only 15 people for the course.
 // 5 already enlisted last semester
-var sliced = list.slice(0,10);
+// var sliced = list.slice(0, 15);
 // log the winners
-console.log("And the Winners are:\n\n"+ sliced +"\nwoohoo! \\o/\n");
+console.log('And the Winners are:\n\n' + list + '\nwoohoo! \\o/\n');
 
 // prepare it for markdown posting in incom.org again
-for(var i = 0; i < sliced.length;i++){
-  sliced[i] = "- " +sliced[i] + "  ";
+var res = [];
+for (var i = 0; i < list.length; i++) {
+  res.push((i + 1) + '. ' + list[i] + '  ');
+  if (i === 14) {
+    res.push('\n  ---  \n  ');
+  }
 }
 
 // write to file
 // http://stackoverflow.com/questions/17324797/write-in-a-text-file-without-overwriting-in-fs-node-js
-fs.writeFileSync("result.txt", sliced.join("\n"), "UTF-8",{'flags': 'w+'});
+fs.writeFileSync('result.txt', res.join('\n'), 'UTF-8', {
+  flags: 'w+'
+});
